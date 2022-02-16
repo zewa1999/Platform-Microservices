c4c777e87fff9200754030ed2d149f7dc58c3b3f
Andrei Costache <costacheandrei43@gmail.com>
2022-02-16 21:53:30 +0200

    Added dbcontext content added services in DI container in program.cs for dbcontext

--- PlatformService/PlatformService/Data/AppDbContext.cs
+++ PlatformService/PlatformService/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace PlatformService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
     }
 }
--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+opt.UseInMemoryDatabase("InMemoryDB"));
+builder.Services.AddControllers();
 
 // Add services to the container.
 
@@ -6,27 +14,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index) ,
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-});
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
