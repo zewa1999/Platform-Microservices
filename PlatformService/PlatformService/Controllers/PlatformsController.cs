efe28900d1c29d80739538edb849e89f3b683cce
Andrei Costache <costacheandrei43@gmail.com>
2022-05-13 13:34:49 +0300

    Created controller and added get method

---
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Repository;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo<Platform> _repo;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepo<Platform> repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("Getting platforms...");
+
+        var platformItem = _repo.Get(orderBy: x => x.OrderBy(p => p.Name));
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+    }
+}
